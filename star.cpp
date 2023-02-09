ec7bdb58c491d49312ccd5a786a11c2d49a6d803
Gaurav Kumar <118988496+gaurav707026@users.noreply.github.com>
2023-02-09 16:11:11 +0530

    Add files via upload

---
+++ star.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // print pascle triangle
+    cout<<"enter the input"<<endl;
+    int n;
+    cin>>n;
+    for (int j = 1; j<=n; j++)
+    {
+        for (int i = 1; i <=3*n-j+1; i++)
+        {
+            cout<<" ";
+        }
+        for (int i = 1; i <= j; i++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    for (int j = 1; j<=n; j++)
+    {
+        for (int i = 1; i <=j-1; i++)
+        {
+            cout<<" ";
+        }
+        for (int i = 1; i <=3*n-j+2; i++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }   
+    for (int j = 0; j <= n; j++)
+    {
+        for (int i = 0; i < n-j; i++)
+        {
+            cout<<" ";
+        }
+        for (int i = 1; i <= 2*n+j+1; i++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+        
+    }
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <=2*n+j; i++)
+        {
+            cout<<" ";
+        }
+        for (int i = 1; i <= n-j+1; i++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+}
